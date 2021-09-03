6f5aebd073e63d998df901980936f5a79d8a1587
Sean Russell <emailseanrussell@gmail.com>
2021-09-03 11:41:03 -0700

    updating files, making sure Im on the right branch

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,7 +62,7 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
+  // delete one tag by its `id` value
   try {
     const tagData = await Tag.destroy({
       where: {
